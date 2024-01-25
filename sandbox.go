2538ea09d7fa8a82ddd5d6e124d68a8ecfd6ac96
Nikita Belenkov <bna.gorzar@gmail.com>
2024-01-25 05:27:18 +0300

    A tout of Go n5: functions - naked return

--- sandbox.go
+++ sandbox.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 )
 
-func swap(x, y string) (string, string) {
-	return y, x
+// function spit splits the given number in two numbers in a proportional rate
+func split(sum int) (x, y int) {
+	x = sum * 1 / 3
+	y = sum - x
+	return
 }
 
 func main() {
-	firstWord, secondWord := swap("Fuck", "You")
-	fmt.Println(firstWord, secondWord)
+	fmt.Println(split(100))
 }
